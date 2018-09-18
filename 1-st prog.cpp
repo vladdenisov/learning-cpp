699e7d5e95fe8e3fcb515eb2532230cda7b1821e
Vlad Denisov <ipovlad.yt@gmail.com>
2018-09-18 21:29:28 +0300

    Updated
    Added selecting array length. Added message if programs didn't found duplicates. Used vector instead of an array.

--- 1-st prog.cpp
+++ 1-st prog.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-#ifdef _WIN32
-    #define WINPAUSE system("pause")
-#endif
 using namespace std;
 int main() {
-	int b[10]; 
-	for (int i = 0; i < 10; i++) {
-	cin >> b[i];
+	int arr_length; 
+	cout << "Enter array length:" << endl; 
+	cin >> arr_length;
+	cout << "Enter "<< arr_length << " array items (Only Nums!)" << endl;
+	vector<int> b(arr_length);
+	for (int i = 0; i < arr_length; i++) {
+		cin >> b[i];
 	}
-	cout << "Searching.."<< endl;
+	cout << "Searching for duplicates.." << endl;
 	vector<int> c;
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < arr_length; i++) {
 		int a = 0;
 		int val = b[i];
-		for (int l = i +1; l < 10; l++) {
+		for (int l = i + 1; l < arr_length; l++) {
 			if (b[i] == b[l]) {
-				a = a + 1; 
-			} 
+				a = a + 1;
+			}
 		}
 		if (a) {
-		if (find(c.begin(), c.end(), val) != c.end() ) {}
-		else {
-			c.push_back(b[i]);
-			cout << b[i] << " repeats for " << a + 1 << " times" << endl; }}
+			if (find(c.begin(), c.end(), val) != c.end()) {}
+			else {
+				c.push_back(b[i]);
+				cout << b[i] << " repeats for " << a + 1 << " times" << endl;
+			}
+		}
 	}
+	if (c.size() == 0) {cout << "Nothing Found" << endl;}
 	system("pause");
 	return 0;
 }
